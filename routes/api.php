78defe540139b562dfdaf44c40f2b2a568e2f3c5
Miguel5858 <jmti4884@gmail.com>
2024-08-23 21:57:35 -0500

    rutas

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \Lomkit\Rest\Facades\Rest;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Rest::resource('products', ProductsController::class);
